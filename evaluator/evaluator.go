3ab07f09574f73a677daa617f28230aeefa1458b
leoDesilva <46300158+leoDesilva@users.noreply.github.com>
2021-11-01 14:17:35 +0000

    environment + setup evaluator + switch type assertions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,5 +1,52 @@
 package evaluator
 
-type Evaluator struct{
+import (
+	"fmt"
+	"forth/lexer"
+	"forth/parser"
+)
 
+type Environment struct {
+	Variables map[string]interface{}
 }
+
+type Evaluator struct{
+	ast parser.ProgramNode
+	environment *Environment
+	position int 
+	node interface{}
+}
+
+func New(ast parser.ProgramNode, environment *Environment) *Evaluator {
+	e := &Evaluator{ast: ast, position: 0, node:ast.Statemets[0], environment: environment}
+	return e
+}
+
+func (e *Evaluator) advance(){
+	e.position++
+
+	if e.position < len(e.ast.Statemets) {
+		e.node = e.ast.Statemets[e.position]
+	}
+}
+
+func (e *Evaluator) Evaluate() bool{
+	for e.position < len(e.ast.Statemets) {
+		e.Eval()
+		e.advance()
+	}
+	return false
+}
+
+func (e *Evaluator) Eval() {
+	switch node := e.node.(type) {
+	case parser.WhileNode:
+		fmt.Println(node,"WHILE")
+	case parser.IfNode:
+		fmt.Println(node,"IF")
+	case lexer.Token:
+		fmt.Println(node,"Token")
+			 
+	}
+}
+
--- forth.go
+++ forth.go
@@ -3,17 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"os"
+	"forth/evaluator"
 	"forth/lexer"
 	"forth/parser"
-  //"forth/evaluator"
+	"io"
+	"os"
 )
+
 
 const PROMPT = ">>"
 
 func startRepl(in io.Reader, out io.Writer) {
     scanner := bufio.NewScanner(in)
+    environment := &evaluator.Environment{Variables: make(map[string]interface{})}
 
     for {
         fmt.Fprintf(out,PROMPT)
@@ -27,13 +29,14 @@ func startRepl(in io.Reader, out io.Writer) {
 
         l := lexer.New(line)
         tokens := l.Lex()
-        fmt.Println(tokens)
 
         p := parser.New(tokens)
         ast, err := p.Parse()
         if err {continue}
-        fmt.Println(ast)
 
+        e := evaluator.New(ast,environment)
+        err = e.Evaluate()
+        if err {continue}
     }
 }
 
